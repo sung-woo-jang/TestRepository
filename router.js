1b7133ee07d24dda12c0585d2de42d484235ea2c
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-08-22 18:24:30 +0900

    Url 읽어내기

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathname) {
+  console.log('pathname: ' + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 let http = require('http');
-
-function start() {
+let url = require('url');
+function start(route) {
   function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    route(pathname);
+
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write('Hello Node.js');
     response.end();
